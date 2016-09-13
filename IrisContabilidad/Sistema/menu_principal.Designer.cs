ed288a6a313ff3d3842269bb0f270da6271455c9
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-09-13 18:53:33 -0400

    commit

--- IrisContabilidad/Sistema/menu_principal.Designer.cs
+++ IrisContabilidad/Sistema/menu_principal.Designer.cs
@@ -30,23 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu_principal));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -108,102 +97,18 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.flowLayoutPanel1.Controls.Add(this.button4);
-            this.flowLayoutPanel1.Controls.Add(this.button9);
-            this.flowLayoutPanel1.Controls.Add(this.button5);
-            this.flowLayoutPanel1.Controls.Add(this.button6);
-            this.flowLayoutPanel1.Controls.Add(this.button7);
-            this.flowLayoutPanel1.Controls.Add(this.button8);
-            this.flowLayoutPanel1.Controls.Add(this.button10);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 55);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
             this.flowLayoutPanel1.Size = new System.Drawing.Size(181, 525);
             this.flowLayoutPanel1.TabIndex = 7;
             // 
-            // button4
-            // 
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(8, 8);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 112);
-            this.button4.TabIndex = 8;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.BackgroundImage = global::IrisContabilidad.Properties.Resources.categoria;
-            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button9.Location = new System.Drawing.Point(8, 126);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(148, 112);
-            this.button9.TabIndex = 13;
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(8, 244);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(148, 112);
-            this.button5.TabIndex = 9;
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
-            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(8, 362);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(148, 112);
-            this.button6.TabIndex = 10;
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
-            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Location = new System.Drawing.Point(8, 480);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(148, 112);
-            this.button7.TabIndex = 11;
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
-            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.Location = new System.Drawing.Point(8, 598);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(148, 112);
-            this.button8.TabIndex = 12;
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
-            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button10.Location = new System.Drawing.Point(8, 716);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(148, 112);
-            this.button10.TabIndex = 14;
-            this.button10.UseVisualStyleBackColor = true;
-            // 
             // button11
             // 
             this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
             this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button11.Location = new System.Drawing.Point(199, 55);
+            this.button11.Location = new System.Drawing.Point(3, 3);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(109, 99);
             this.button11.TabIndex = 15;
@@ -215,80 +120,32 @@
             this.button12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button12.BackgroundImage")));
             this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button12.Location = new System.Drawing.Point(314, 55);
+            this.button12.Location = new System.Drawing.Point(118, 3);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(109, 99);
             this.button12.TabIndex = 16;
             this.button12.UseVisualStyleBackColor = true;
             this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
-            // button13
+            // flowLayoutPanel2
             // 
-            this.button13.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button13.BackgroundImage")));
-            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button13.Location = new System.Drawing.Point(429, 55);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(109, 99);
-            this.button13.TabIndex = 17;
-            this.button13.UseVisualStyleBackColor = true;
-            // 
-            // button14
-            // 
-            this.button14.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button14.BackgroundImage")));
-            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button14.Location = new System.Drawing.Point(544, 55);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(109, 99);
-            this.button14.TabIndex = 18;
-            this.button14.UseVisualStyleBackColor = true;
-            // 
-            // button15
-            // 
-            this.button15.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button15.BackgroundImage")));
-            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button15.Location = new System.Drawing.Point(659, 55);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(109, 99);
-            this.button15.TabIndex = 19;
-            this.button15.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            this.button16.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button16.BackgroundImage")));
-            this.button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button16.Location = new System.Drawing.Point(774, 55);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(109, 99);
-            this.button16.TabIndex = 20;
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button17.BackgroundImage")));
-            this.button17.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button17.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button17.Location = new System.Drawing.Point(889, 55);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(109, 99);
-            this.button17.TabIndex = 21;
-            this.button17.UseVisualStyleBackColor = true;
+            this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Controls.Add(this.button11);
+            this.flowLayoutPanel2.Controls.Add(this.button12);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(199, 55);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(831, 527);
+            this.flowLayoutPanel2.TabIndex = 22;
             // 
             // menu_principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1042, 649);
-            this.Controls.Add(this.button17);
-            this.Controls.Add(this.button16);
-            this.Controls.Add(this.button15);
-            this.Controls.Add(this.button14);
-            this.Controls.Add(this.button13);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
+            this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.MaximizeBox = true;
             this.MinimizeBox = true;
@@ -297,7 +154,6 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.menu_principal_Load);
             this.Controls.SetChildIndex(this.flowLayoutPanel1, 0);
-            this.Controls.SetChildIndex(this.button11, 0);
             this.Controls.SetChildIndex(this.panel2, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
             this.Controls.SetChildIndex(this.label1, 0);
@@ -305,16 +161,11 @@
             this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.claveText, 0);
             this.Controls.SetChildIndex(this.linkLabel1, 0);
-            this.Controls.SetChildIndex(this.button12, 0);
-            this.Controls.SetChildIndex(this.button13, 0);
-            this.Controls.SetChildIndex(this.button14, 0);
-            this.Controls.SetChildIndex(this.button15, 0);
-            this.Controls.SetChildIndex(this.button16, 0);
-            this.Controls.SetChildIndex(this.button17, 0);
+            this.Controls.SetChildIndex(this.flowLayoutPanel2, 0);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -323,19 +174,8 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button13;
-        private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
--- IrisContabilidad/Sistema/menu_principal.cs
+++ IrisContabilidad/Sistema/menu_principal.cs
@@ -12,6 +12,14 @@ namespace IrisContabilidad.Sistema
 {
     public partial class menu_principal : FormBase
     {
+
+
+
+        //variables
+        string codigoModuloPresionado = "";
+        string codigoVentanaPresionada = "";
+
+
         public menu_principal()
         {
             InitializeComponent();
@@ -24,7 +32,44 @@ namespace IrisContabilidad.Sistema
         }
         public override void loadVentana()
         {
-            
+            try
+            {
+                string carpetaImagenes = Directory.GetCurrentDirectory();
+                carpetaImagenes += @"\Resources\Imagenes\";
+                MessageBox.Show(carpetaImagenes);
+                //cargar modulos
+                string sql = "select codigo,descripcion,nombre_logico,imagen from sistema_modulos where estado='1'";
+                DataSet ds = Clases.utilidades.ejecutarcomando(sql);
+                foreach (DataRow rowModulo in ds.Tables[0].Rows)
+                {
+                    Button botonModulo = new Button();
+                    botonModulo.Width = 148;
+                    botonModulo.Height = 112;
+                    if (rowModulo[3].ToString() != "")
+                    {
+                        botonModulo.BackgroundImage = Image.FromFile(carpetaImagenes + rowModulo[3].ToString());
+                    }
+                    botonModulo.Tag = rowModulo[0].ToString();
+                    flowLayoutPanel1.Controls.Add(botonModulo);
+
+
+                    //cargar ventana de cada modulo
+                    //string cmd = "select codigo,descripcion,cod_modulo,nombre_logico,imagen from sistema_modulo_opciones where estado='1' and cod_modulo='1'";
+                    //DataSet ds2 = Clases.utilidades.ejecutarcomando(cmd);
+                    //Button botonVentana = new Button();
+                    //botonVentana.Width = 148;
+                    //botonVentana.Height = 112;
+                    //if (ds2.Tables[0].Rows[0][4].ToString() != "")
+                    //{
+                    //    botonVentana.BackgroundImage = Image.FromFile(carpetaImagenes + ds2.Tables[0].Rows[0][4].ToString());
+                    //}
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: "+ex.ToString());
+            }
             this.tituloLabel.Text = "Menú principal";
         }
 
@@ -41,6 +86,11 @@ namespace IrisContabilidad.Sistema
             Object objeto = Activator.CreateInstance(tipoObjeto);
             ventana = (Form)objeto;
             ventana.Show();            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
