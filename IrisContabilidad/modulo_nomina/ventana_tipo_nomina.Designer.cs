c159bbe1a327f9332160acfb51742d45a22c362f
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-12-25 02:26:15 -0400

    commit

--- IrisContabilidad/modulo_nomina/ventana_tipo_nomina.Designer.cs
+++ IrisContabilidad/modulo_nomina/ventana_tipo_nomina.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventana_tipo_nomina));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.NominaTipoText = new System.Windows.Forms.TextBox();
+            this.activoCheck = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NominatipoIdText = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 363);
-            this.panel1.Size = new System.Drawing.Size(620, 54);
+            this.panel1.Location = new System.Drawing.Point(16, 307);
+            this.panel1.Size = new System.Drawing.Size(532, 54);
             // 
             // button2
             // 
@@ -45,33 +56,139 @@
             // button1
             // 
             this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Location = new System.Drawing.Point(479, 5);
+            this.button1.Location = new System.Drawing.Point(391, 5);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel2
             // 
-            this.panel2.Size = new System.Drawing.Size(631, 21);
+            this.panel2.Size = new System.Drawing.Size(567, 21);
             // 
             // button3
             // 
             this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.Location = new System.Drawing.Point(240, 5);
+            this.button3.Location = new System.Drawing.Point(196, 5);
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.NominaTipoText);
+            this.groupBox2.Controls.Add(this.activoCheck);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(16, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(532, 161);
+            this.groupBox2.TabIndex = 26;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // NominaTipoText
+            // 
+            this.NominaTipoText.BackColor = System.Drawing.Color.White;
+            this.NominaTipoText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NominaTipoText.Location = new System.Drawing.Point(163, 54);
+            this.NominaTipoText.MaxLength = 200;
+            this.NominaTipoText.Name = "NominaTipoText";
+            this.NominaTipoText.Size = new System.Drawing.Size(256, 26);
+            this.NominaTipoText.TabIndex = 14;
+            // 
+            // activoCheck
+            // 
+            this.activoCheck.AutoSize = true;
+            this.activoCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.activoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activoCheck.Location = new System.Drawing.Point(163, 118);
+            this.activoCheck.Name = "activoCheck";
+            this.activoCheck.Size = new System.Drawing.Size(68, 21);
+            this.activoCheck.TabIndex = 21;
+            this.activoCheck.Text = "Activo";
+            this.activoCheck.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Nombre:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.NominatipoIdText);
+            this.groupBox1.Location = new System.Drawing.Point(16, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 80);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(378, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 37);
+            this.button4.TabIndex = 24;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipo nomina";
+            // 
+            // NominatipoIdText
+            // 
+            this.NominatipoIdText.BackColor = System.Drawing.Color.SkyBlue;
+            this.NominatipoIdText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NominatipoIdText.Location = new System.Drawing.Point(189, 31);
+            this.NominatipoIdText.Name = "NominatipoIdText";
+            this.NominatipoIdText.Size = new System.Drawing.Size(183, 26);
+            this.NominatipoIdText.TabIndex = 0;
             // 
             // ventana_tipo_nomina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 429);
+            this.ClientSize = new System.Drawing.Size(567, 373);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ventana_tipo_nomina";
             this.Text = "tipo_nomina";
             this.Load += new System.EventHandler(this.ventana_tipo_nomina_Load);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.groupBox2, 0);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox NominaTipoText;
+        private System.Windows.Forms.CheckBox activoCheck;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NominatipoIdText;
     }
 }
--- IrisContabilidad/modulo_nomina/ventana_tipo_nomina.cs
+++ IrisContabilidad/modulo_nomina/ventana_tipo_nomina.cs
@@ -7,17 +7,141 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IrisContabilidad.clases;
+using IrisContabilidad.modelos;
 using IrisContabilidad.modulo_sistema;
 
 namespace IrisContabilidad.modulo_nomina
 {
     public partial class ventana_tipo_nomina : formBase
     {
+
+
+        //objetos
+        private nomina_tipo nomina_tipo;
+        utilidades utilidades = new utilidades();
+        private singleton singleton = new singleton();
+        private empleado empleado;
+
+
+        //modelos
+        modeloNominaTipo modeloNominaTipo = new modeloNominaTipo();
+
+
+
         public ventana_tipo_nomina()
         {
             InitializeComponent();
+            empleado = singleton.getEmpleado();
+            this.tituloLabel.Text = utilidades.GetTituloVentana(empleado, "ventana tipos de nómina");
+            this.Text = tituloLabel.Text;
+            loadVentana();
         }
+        public void loadVentana()
+        {
+            try
+            {
+                if (nomina_tipo != null)
+                {
+                    NominatipoIdText.Text = nomina_tipo.codigo.ToString();
+                    NominaTipoText.Text = nomina_tipo.nombre;
+                    activoCheck.Checked = Convert.ToBoolean(nomina_tipo.activo);
+                }
+                else
+                {
+                    NominatipoIdText.Text = "";
+                    NominaTipoText.Text = "";
+                    activoCheck.Checked = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loadVentana.: " + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public bool ValidarGetAction()
+        {
+            try
+            {
+                if (NominaTipoText.Text == "")
+                {
+                    MessageBox.Show("Falta el nombre ", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    NominaTipoText.Focus();
+                    NominaTipoText.SelectAll();
+                    return false;
+                }
 
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ValidarGetAction.: " + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        public void GetAction()
+        {
+            try
+            {
+
+                if (MessageBox.Show("Desea guardar?", "", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    return;
+                }
+
+                if (!ValidarGetAction())
+                {
+                    return;
+                }
+
+
+                bool crear = false;
+                if (nomina_tipo == null)
+                {
+                    //agrega
+                    crear = true;
+                    nomina_tipo = new nomina_tipo();
+                    nomina_tipo.codigo = modeloNominaTipo.getNext();
+                }
+
+                nomina_tipo.nombre = NominaTipoText.Text;
+                nomina_tipo.activo = Convert.ToBoolean(activoCheck.Checked);
+
+                if (crear)
+                {
+                    //agrega
+                    if (modeloNominaTipo.agregarNominaTipo(nomina_tipo) == true)
+                    {
+                        MessageBox.Show("Se agregó", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se agregó", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    //actualiza
+                    if (modeloNominaTipo.modificarNominaTipo(nomina_tipo) == true)
+                    {
+                        MessageBox.Show("Se modificó", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se modificó", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+                nomina_tipo = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error GetAction.: " + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+       
         private void ventana_tipo_nomina_Load(object sender, EventArgs e)
         {
 
@@ -33,6 +157,25 @@ namespace IrisContabilidad.modulo_nomina
             {
                 this.Close();
             }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea guardar?", "", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                GetAction();
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            nomina_tipo = null;
+            loadVentana();
         }
     }
 }
