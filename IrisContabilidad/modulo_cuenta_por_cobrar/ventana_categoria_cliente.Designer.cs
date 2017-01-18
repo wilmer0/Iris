70fb67e11d30af388076869ca3077059dde170c0
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2017-01-18 01:01:19 -0400

    commit

--- IrisContabilidad/modulo_cuenta_por_cobrar/ventana_categoria_cliente.Designer.cs
+++ IrisContabilidad/modulo_cuenta_por_cobrar/ventana_categoria_cliente.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace IrisContabilidad.modulo_facturacion
+﻿namespace IrisContabilidad.modulo_cuenta_por_cobrar
 {
-    partial class ventana_tipo_comprobante_fiscal
+    partial class ventana_categoria_cliente
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventana_tipo_comprobante_fiscal));
-            this.secuenciaText = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventana_categoria_cliente));
             this.activoCheck = new System.Windows.Forms.CheckBox();
             this.nombreText = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,49 +43,26 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 331);
-            this.panel1.Size = new System.Drawing.Size(559, 54);
+            this.panel1.Location = new System.Drawing.Point(12, 284);
+            this.panel1.Size = new System.Drawing.Size(513, 54);
             // 
             // button2
             // 
             this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Location = new System.Drawing.Point(418, 5);
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Location = new System.Drawing.Point(372, 5);
             // 
             // panel2
             // 
-            this.panel2.Size = new System.Drawing.Size(583, 21);
+            this.panel2.Size = new System.Drawing.Size(537, 21);
             // 
             // button3
             // 
             this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.Location = new System.Drawing.Point(209, 5);
-            this.button3.Click += new System.EventHandler(this.button3_Click_1);
-            // 
-            // secuenciaText
-            // 
-            this.secuenciaText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.secuenciaText.Location = new System.Drawing.Point(195, 187);
-            this.secuenciaText.MaxLength = 2;
-            this.secuenciaText.Name = "secuenciaText";
-            this.secuenciaText.Size = new System.Drawing.Size(253, 26);
-            this.secuenciaText.TabIndex = 84;
-            this.secuenciaText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.secuenciaText_KeyDown);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(99, 190);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 20);
-            this.label1.TabIndex = 83;
-            this.label1.Text = "secuencia";
+            this.button3.Location = new System.Drawing.Point(186, 5);
             // 
             // activoCheck
             // 
@@ -96,33 +71,30 @@
             this.activoCheck.CheckState = System.Windows.Forms.CheckState.Checked;
             this.activoCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.activoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.activoCheck.Location = new System.Drawing.Point(195, 236);
+            this.activoCheck.Location = new System.Drawing.Point(174, 196);
             this.activoCheck.Name = "activoCheck";
             this.activoCheck.Size = new System.Drawing.Size(68, 21);
-            this.activoCheck.TabIndex = 82;
+            this.activoCheck.TabIndex = 88;
             this.activoCheck.Text = "Activo";
             this.activoCheck.UseVisualStyleBackColor = true;
-            this.activoCheck.KeyDown += new System.Windows.Forms.KeyEventHandler(this.activoCheck_KeyDown);
             // 
             // nombreText
             // 
             this.nombreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nombreText.Location = new System.Drawing.Point(195, 134);
+            this.nombreText.Location = new System.Drawing.Point(174, 135);
             this.nombreText.MaxLength = 30;
             this.nombreText.Name = "nombreText";
             this.nombreText.Size = new System.Drawing.Size(253, 26);
-            this.nombreText.TabIndex = 81;
-            this.nombreText.TextChanged += new System.EventHandler(this.nombreText_TextChanged);
-            this.nombreText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nombreText_KeyDown);
+            this.nombreText.TabIndex = 87;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(118, 137);
+            this.label3.Location = new System.Drawing.Point(97, 138);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 20);
-            this.label3.TabIndex = 80;
+            this.label3.TabIndex = 86;
             this.label3.Text = "Nombre";
             // 
             // groupBox1
@@ -132,63 +104,58 @@
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(559, 66);
-            this.groupBox1.TabIndex = 79;
+            this.groupBox1.Size = new System.Drawing.Size(514, 66);
+            this.groupBox1.TabIndex = 85;
             this.groupBox1.TabStop = false;
             // 
             // tipoComprobanteIdText
             // 
             this.tipoComprobanteIdText.BackColor = System.Drawing.Color.SkyBlue;
             this.tipoComprobanteIdText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tipoComprobanteIdText.Location = new System.Drawing.Point(183, 22);
+            this.tipoComprobanteIdText.Location = new System.Drawing.Point(162, 22);
             this.tipoComprobanteIdText.Name = "tipoComprobanteIdText";
             this.tipoComprobanteIdText.Size = new System.Drawing.Size(155, 26);
             this.tipoComprobanteIdText.TabIndex = 18;
-            this.tipoComprobanteIdText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tipoComprobanteIdText_KeyDown);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 25);
+            this.label2.Location = new System.Drawing.Point(69, 24);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.Size = new System.Drawing.Size(87, 20);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Tipo comprobante";
+            this.label2.Text = "Categoria";
             // 
             // button4
             // 
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(344, 17);
+            this.button4.Location = new System.Drawing.Point(323, 17);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(47, 37);
             this.button4.TabIndex = 23;
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // ventana_tipo_comprobante_fiscal
+            // ventana_categoria_cliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 397);
-            this.Controls.Add(this.secuenciaText);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(537, 350);
             this.Controls.Add(this.activoCheck);
             this.Controls.Add(this.nombreText);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox1);
-            this.Name = "ventana_tipo_comprobante_fiscal";
-            this.Text = "ventana_tipo_comprobante_fiscal";
-            this.Load += new System.EventHandler(this.ventana_tipo_comprobante_fiscal_Load);
+            this.Name = "ventana_categoria_cliente";
+            this.Text = "ventana_categoria_cliente";
+            this.Load += new System.EventHandler(this.ventana_categoria_cliente_Load);
             this.Controls.SetChildIndex(this.panel1, 0);
             this.Controls.SetChildIndex(this.panel2, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.nombreText, 0);
             this.Controls.SetChildIndex(this.activoCheck, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.secuenciaText, 0);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
@@ -201,8 +168,6 @@
 
         #endregion
 
-        public System.Windows.Forms.TextBox secuenciaText;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox activoCheck;
         public System.Windows.Forms.TextBox nombreText;
         private System.Windows.Forms.Label label3;
