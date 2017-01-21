c75302c7131130103a4e80a24f30d8bcbfa66f39
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2017-01-20 20:41:12 -0400

    commit

---
+++ IrisContabilidad/modulo_opciones/ventana_carga_documentos.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace IrisContabilidad.modulo_opciones
+{
+    partial class ventana_carga_documentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ventana_carga_documentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "ventana_carga_documentos";
+            this.Text = "ventana_carga_documentos";
+            this.Load += new System.EventHandler(this.ventana_carga_documentos_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
