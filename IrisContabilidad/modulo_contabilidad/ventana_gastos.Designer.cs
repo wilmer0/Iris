c8a5fd50da128ef8265403134a8362aace09c040
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2017-02-15 01:29:23 -0400

    commit

---
+++ IrisContabilidad/modulo_contabilidad/ventana_gastos.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace IrisContabilidad.modulo_contabilidad
+{
+    partial class ventana_gastos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ventana_gastos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "ventana_gastos";
+            this.Text = "ventana_gastos";
+            this.Load += new System.EventHandler(this.ventana_gastos_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ IrisContabilidad/modulo_contabilidad/ventana_gastos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IrisContabilidad.modulo_contabilidad
+{
+    public partial class ventana_gastos : Form
+    {
+        public ventana_gastos()
+        {
+            InitializeComponent();
+        }
+
+        private void ventana_gastos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
