778c08942b6229221d9d1af00c48b6052bad4e44
Wilmer De La Rosa <wilmer.delarosa.santana@gmail.com>
2017-05-15 14:57:50 -0400

    commit

--- IrisContabilidad/modulo_contabilidad/ventana_parametrizacion_contable.cs
+++ IrisContabilidad/modulo_contabilidad/ventana_parametrizacion_contable.cs
@@ -7,20 +7,104 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IrisContabilidad.clases;
+using IrisContabilidad.clases_reportes;
+using IrisContabilidad.clases_reportes_modelos;
+using IrisContabilidad.modelos;
 using IrisContabilidad.modulo_sistema;
 
 namespace IrisContabilidad.modulo_contabilidad
 {
     public partial class ventana_parametrizacion_contable : formBase
     {
+
+        //objetos
+        utilidades utilidades = new utilidades();
+        singleton singleton = new singleton();
+        empleado empleado;
+        private parametrizacion_contable parametrizacionContable;
+
+        //modelos
+        modeloCuentaContable modeloCuentaContable=new modeloCuentaContable();
+
+
         public ventana_parametrizacion_contable()
         {
             InitializeComponent();
+            empleado = singleton.getEmpleado();
+            this.tituloLabel.Text = utilidades.GetTituloVentana(empleado, "ventana parametrización contable");
+            this.Text = tituloLabel.Text;
+            loadVentana();
+        }
+
+        public void loadVentana()
+        {
+            try
+            {
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loadVentana.:" + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public bool validarGetAcion()
+        {
+            try
+            {
+                
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error validarGetAcion.:" + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
         }
 
         private void ventana_parametrizacion_contable_Load(object sender, EventArgs e)
         {
 
         }
+
+        public void getAction()
+        {
+            try
+            {
+                if (!validarGetAcion())
+                {
+                    return;
+                }
+                if (MessageBox.Show("Desea procesar?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    return;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getAction.:" + ex.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void salir()
+        {
+            if (MessageBox.Show("Desea salir?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
