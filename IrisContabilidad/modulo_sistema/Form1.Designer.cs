56e4240a17071eaac1f67fa55d5a16810aca34ce
Wilmer De La Rosa <wilmer.delarosa.santana@gmail.com>
2017-06-01 22:20:43 -0400

    commit

--- IrisContabilidad/modulo_sistema/Form1.Designer.cs
+++ IrisContabilidad/modulo_sistema/Form1.Designer.cs
@@ -33,157 +33,125 @@ namespace IrisContabilidad
         /// </summary>
         private void InitializeComponent()
         {
-            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
-            this.label1 = new Label();
-            this.panel3 = new Panel();
-            this.groupBox1 = new GroupBox();
-            this.label2 = new Label();
-            this.usuarioText = new TextBox();
-            this.claveText = new TextBox();
-            this.label3 = new Label();
-            this.button4 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.claveText = new System.Windows.Forms.TextBox();
+            this.usuarioText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new Point(12, 375);
-            this.panel1.Size = new Size(587, 54);
+            this.panel1.Location = new System.Drawing.Point(12, 331);
+            this.panel1.Size = new System.Drawing.Size(650, 54);
             // 
             // button2
             // 
             this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.Click += new EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // button1
             // 
             this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Location = new Point(446, 5);
-            this.button1.Click += new EventHandler(this.button1_Click);
+            this.button1.Location = new System.Drawing.Point(509, 5);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // panel2
             // 
-            this.panel2.Size = new Size(611, 21);
+            this.panel2.Size = new System.Drawing.Size(674, 21);
             // 
             // button3
             // 
             this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.Location = new Point(222, 5);
-            this.button3.Click += new EventHandler(this.button3_Click);
+            this.button3.Location = new System.Drawing.Point(255, 5);
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(241, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(220, 173);
+            this.panel4.TabIndex = 9;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.claveText);
+            this.groupBox2.Controls.Add(this.usuarioText);
+            this.groupBox2.Location = new System.Drawing.Point(16, 206);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(643, 111);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // claveText
+            // 
+            this.claveText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.claveText.Location = new System.Drawing.Point(225, 62);
+            this.claveText.MaxLength = 30;
+            this.claveText.Name = "claveText";
+            this.claveText.PasswordChar = '*';
+            this.claveText.Size = new System.Drawing.Size(220, 29);
+            this.claveText.TabIndex = 1;
+            this.claveText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.claveText_KeyDown_1);
+            // 
+            // usuarioText
+            // 
+            this.usuarioText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuarioText.Location = new System.Drawing.Point(225, 19);
+            this.usuarioText.MaxLength = 30;
+            this.usuarioText.Name = "usuarioText";
+            this.usuarioText.Size = new System.Drawing.Size(220, 29);
+            this.usuarioText.TabIndex = 0;
+            this.usuarioText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.usuarioText_KeyDown_1);
+            this.usuarioText.KeyUp += new System.Windows.Forms.KeyEventHandler(this.usuarioText_KeyUp);
+            this.usuarioText.Leave += new System.EventHandler(this.usuarioText_Leave);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new Point(13, 355);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new Size(193, 17);
-            this.label1.TabIndex = 18;
-            this.label1.Text = "Developed by Black Code";
-            // 
-            // panel3
-            // 
-            this.panel3.BackgroundImage = ((Image)(resources.GetObject("panel3.BackgroundImage")));
-            this.panel3.BackgroundImageLayout = ImageLayout.Stretch;
-            this.panel3.Location = new Point(207, 27);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new Size(224, 199);
-            this.panel3.TabIndex = 17;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.usuarioText);
-            this.groupBox1.Controls.Add(this.claveText);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new Point(16, 232);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new Size(580, 102);
-            this.groupBox1.TabIndex = 16;
-            this.groupBox1.TabStop = false;
+            this.label1.Size = new System.Drawing.Size(103, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "user name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new Point(126, 19);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new Size(76, 20);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Usuario:";
-            // 
-            // usuarioText
-            // 
-            this.usuarioText.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            this.usuarioText.Location = new Point(208, 19);
-            this.usuarioText.MaxLength = 30;
-            this.usuarioText.Name = "usuarioText";
-            this.usuarioText.Size = new Size(224, 26);
-            this.usuarioText.TabIndex = 11;
-            this.usuarioText.KeyDown += new KeyEventHandler(this.usuarioText_KeyDown);
-            // 
-            // claveText
-            // 
-            this.claveText.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            this.claveText.Location = new Point(208, 62);
-            this.claveText.MaxLength = 30;
-            this.claveText.Name = "claveText";
-            this.claveText.PasswordChar = '*';
-            this.claveText.Size = new Size(224, 26);
-            this.claveText.TabIndex = 12;
-            this.claveText.KeyDown += new KeyEventHandler(this.claveText_KeyDown);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new Point(133, 62);
-            this.label3.Name = "label3";
-            this.label3.Size = new Size(58, 20);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Clave:";
-            // 
-            // button4
-            // 
-            this.button4.Anchor = AnchorStyles.None;
-            this.button4.BackColor = Color.MediumSeaGreen;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = FlatStyle.Popup;
-            this.button4.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = Color.White;
-            this.button4.Location = new Point(454, 340);
-            this.button4.Name = "button4";
-            this.button4.Size = new Size(142, 29);
-            this.button4.TabIndex = 19;
-            this.button4.Text = "Ver Información";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new EventHandler(this.button4_Click);
+            this.label2.Size = new System.Drawing.Size(95, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new SizeF(6F, 13F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(611, 441);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.groupBox1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(674, 397);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel4);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.Controls.SetChildIndex(this.panel1, 0);
             this.Controls.SetChildIndex(this.panel2, 0);
-            this.Controls.SetChildIndex(this.groupBox1, 0);
-            this.Controls.SetChildIndex(this.panel3, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.button4, 0);
+            this.Controls.SetChildIndex(this.panel4, 0);
+            this.Controls.SetChildIndex(this.groupBox2, 0);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,14 +159,16 @@ namespace IrisContabilidad
 
         #endregion
 
-        private Label label1;
         private Panel panel3;
         private GroupBox groupBox1;
-        private Label label2;
-        public TextBox usuarioText;
-        public TextBox claveText;
         private Label label3;
         public Button button4;
+        private Panel panel4;
+        private GroupBox groupBox2;
+        private TextBox usuarioText;
+        private TextBox claveText;
+        private Label label2;
+        private Label label1;
     }
 }
 
